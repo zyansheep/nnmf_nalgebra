e5130b5e9f4290d2d42fd6dcd2494c040e3167c7
zyansheep <zyansheep@protonmail.com>
2023-02-20 20:23:02 -0600

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,24 @@
-use nalgebra::{DMatrix, SMatrix};
+use std::{ops::{Sub, Div}, iter::Sum};
 
-pub fn non_negative_matrix_factorization<const R: usize, const C: usize, const K: usize> (
-	matrix: &SMatrix<f64, R, C>,
+use nalgebra::{DMatrix, SMatrix, ComplexField, Scalar};
+use rand::{prelude::Distribution, distributions::Standard};
+
+/// Find the N by K and K by C matrix factors of a staticly-sized matrix R by C.s
+pub fn non_negative_matrix_factorization<T, const R: usize, const C: usize, const K: usize> (
+	matrix: &SMatrix<T, R, C>,
 	max_iter: usize,
-	tolerance: f64,
-) -> (SMatrix<f64, R, K>, SMatrix<f64, K, C>)
+	tolerance: T,
+) -> (SMatrix<T, R, K>, SMatrix<T, K, C>)
 where
-	// T: Scalar + ComplexField<RealField = T> + PartialOrd + Sum<T>,
-	// Standard: Distribution<T>,
+	T: Scalar + ComplexField<RealField = T> + Sub<T> + Clone + Copy + Sum<T> + PartialOrd + Div<T, Output = T>,
+	Standard: Distribution<T>,
 {
 	// Two reduced-dimension vectors we are trying to calculate, each field is initialized to [0, 1)
-	let mut w: SMatrix<f64, R, K> = SMatrix::new_random();
-	let mut h: SMatrix<f64, K, C> = SMatrix::new_random();
+	let mut w: SMatrix<T, R, K> = SMatrix::new_random();
+	let mut h: SMatrix<T, K, C> = SMatrix::new_random();
 
-	let mut w_transpose: SMatrix<f64, K, R> = SMatrix::zeros();
-	let mut h_transpose: SMatrix<f64, C, K> = SMatrix::zeros();
+	let mut w_transpose: SMatrix<T, K, R> = SMatrix::zeros();
+	let mut h_transpose: SMatrix<T, C, K> = SMatrix::zeros();
 
 	// Repeat until convergence
     for _ in 0..max_iter {
@@ -24,46 +28,51 @@ where
 		let cost = matrix
 			.iter()
 			.zip(wh.iter())
-			.map(|(a, b)| (a - b).powi(2))
-			.sum::<f64>();
+			.map(|(a, b)| (*a - *b).powi(2))
+			.sum::<T>();
 
 		if cost < tolerance { break; }
 
 		// Numerator & denominator for h update
 		w.transpose_to(&mut w_transpose);
 
-		let wt_v = w_transpose * matrix;
-		let wt_w_h = w_transpose * wh;
+		let wt_v: SMatrix<T, K, C> = w_transpose * matrix;
+		let wt_w_h: SMatrix<T, K, C> = w_transpose * wh;
 
 		// Component-wise update of h
-		h.iter_mut().zip(wt_v.iter().zip(wt_w_h.iter())).map(|(h_old, (num, den))| *h_old = *h_old * (num / den)).last().unwrap();
+		h.iter_mut().zip(wt_v.iter().zip(wt_w_h.iter())).map(|(h_old, (num, den))| *h_old = *h_old * (*num / *den)).last().unwrap();
 
 		// Numerator & denominator for w update
 		h.transpose_to(&mut h_transpose);
 
-		let v_ht = matrix * h_transpose;
-		let w_h_ht = w * h * h_transpose;
+		let v_ht: SMatrix<T, R, K> = matrix * h_transpose;
+		let w_h_ht: SMatrix<T, R, K> = w * h * h_transpose;
 
 		// Component-wise update of w
-		w.iter_mut().zip(v_ht.iter().zip(w_h_ht.iter())).map(|(w_old, (num, den))| *w_old = *w_old * (num / den)).last().unwrap();
+		w.iter_mut().zip(v_ht.iter().zip(w_h_ht.iter())).map(|(w_old, (num, den))| *w_old = *w_old * (*num / *den)).last().unwrap();
     }
 
 	(w, h)
 }
 
-pub fn non_negative_matrix_factorization_dyn(
-	matrix: &DMatrix<f64>,
+/// Does the same thing as non_negative_matrix_factorization but using heap-allocated matricies. (Oh how I wish statics and dynamics could be abstracted over...)
+pub fn non_negative_matrix_factorization_dyn<T>(
+	matrix: &DMatrix<T>,
 	k: usize,
 	max_iter: usize,
-	tolerance: f64,
-) -> (DMatrix<f64>, DMatrix<f64>) {
+	tolerance: T,
+) -> (DMatrix<T>, DMatrix<T>)
+where
+	T: Scalar + ComplexField<RealField = T> + Sub<T> + Clone + Copy + Sum<T> + PartialOrd + Div<T, Output = T>,
+	Standard: Distribution<T>,
+{
 	let (n_rows, n_cols) = matrix.shape();
 	
-	let w = &mut DMatrix::from_fn(n_rows, k, |_i, _j| rand::random::<f64>());
-	let h = &mut DMatrix::from_fn(k, n_cols, |_i, _j| rand::random::<f64>());
+	let w = &mut DMatrix::new_random(n_rows, k);
+	let h = &mut DMatrix::new_random(k, n_cols);
 
-	let w_transpose = &mut w.clone();
-	let h_transpose = &mut h.clone();
+	let w_transpose = &mut DMatrix::zeros(k, n_rows);
+	let h_transpose = &mut DMatrix::zeros(n_cols, k);
 
 	let wh = &mut (w.clone() * h.clone());
 
@@ -74,19 +83,19 @@ pub fn non_negative_matrix_factorization_dyn(
 		let cost = matrix
 			.iter()
 			.zip(wh.iter())
-			.map(|(a, b)| (a - b).powi(2))
-			.sum::<f64>();
+			.map(|(a, b)| (*a - *b).powi(2))
+			.sum::<T>();
 
 		if cost < tolerance { break; }
 
 		// Numerator & denominator for h update
 		w.transpose_to(w_transpose);
 
-		let wt_v = w_transpose.clone() * matrix;
-		let wt_w_h = w_transpose.clone() * wh.clone();
+		let wt_v = &*w_transpose * matrix;
+		let wt_w_h = &*w_transpose * &*wh;
 
 		// Component-wise update of h
-		h.iter_mut().zip(wt_v.iter().zip(wt_w_h.iter())).map(|(h_old, (num, den))| *h_old = *h_old * (num / den)).last();
+		h.iter_mut().zip(wt_v.iter().zip(wt_w_h.iter())).map(|(h_old, (num, den))| *h_old = *h_old * (*num / *den)).last().unwrap();
 
 		// Numerator & denominator for w update
 		h.transpose_to(h_transpose);
@@ -95,7 +104,7 @@ pub fn non_negative_matrix_factorization_dyn(
 		let w_h_ht = w.clone() * h.clone() * h_transpose.clone();
 
 		// Component-wise update of w
-		w.iter_mut().zip(v_ht.iter().zip(w_h_ht.iter())).map(|(w_old, (num, den))| *w_old = *w_old * (num / den)).last();
+		w.iter_mut().zip(v_ht.iter().zip(w_h_ht.iter())).map(|(w_old, (num, den))| *w_old = *w_old * (*num / *den)).last().unwrap();
 
 		*wh = w.clone() * h.clone();
     }
@@ -106,37 +115,41 @@ pub fn non_negative_matrix_factorization_dyn(
 
 #[cfg(test)]
 mod tests {
-    use nalgebra::{matrix, DMatrix, SMatrix};
+    use nalgebra::{matrix, SMatrix, dmatrix};
 
-    use crate::non_negative_matrix_factorization;
+    use crate::*;
 
 	#[test]
-	fn put_it_to_the_test() {
+	fn test_static_and_dyn() {
 		let matrix: SMatrix<f64, 4, 5> = matrix![
 			1.0, 2.0, 0.0, 30.0, 1.5;
 			0.0, 3.0, 1.0, 30.0, 1.6;
 			5.0, 1.0, 10.0, 30.0, 1.7;
 			3.0, 1.0, 10.0, 30.0, 1.7
 		];
-
-		/* let w: SMatrix<f32, 3, 1> = matrix![
-			2.1;
-			3.2;
-			0.9
-		];
-
-		let h: SMatrix<f32, 1, 5> = matrix![3.2, 2.0, 1.0, 5.0, 3.0]; */
-
-
-		/* assert_eq!(matrix, w * h); */
-		let (w, h) = non_negative_matrix_factorization::<4, 5, 4>(&matrix, 1000, 0.01);
+		
+		let (w, h) = non_negative_matrix_factorization::<_, 4, 5, 3>(&matrix, 1000, 0.01);
 		println!("{w}\n{h}");
 		let prediction = w * h;
 
 		println!("Matrix: {}\n Predic: {}", matrix, prediction);
 		assert!(matrix.relative_eq(&(w * h), 0.5, 0.5));
 
+		let d_matrix: DMatrix<f64> = dmatrix![
+			1.0, 2.0, 0.0, 30.0, 1.5;
+			0.0, 3.0, 1.0, 30.0, 1.6;
+			5.0, 1.0, 10.0, 30.0, 1.7;
+			3.0, 1.0, 10.0, 30.0, 1.7
+		];
+		let (w_dyn, h_dyn) = non_negative_matrix_factorization_dyn::<f64>(&d_matrix, 3, 1000, 0.01);
+		println!("{w}\n{h}");
+		let prediction = w * h;
 
+		println!("Matrix: {}\n Predic: {}", matrix, prediction);
+		assert!(d_matrix.relative_eq(&(w_dyn.clone() * h_dyn.clone()), 0.5, 0.5));
+
+		/* assert_eq!(w, w_dyn);
+		assert_eq!(h, h_dyn); */
 
 		/* let mut nmf = FixedTemplateNmf::new(templates, activation_coef, &input, 0.5);
 
